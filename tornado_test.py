617328d0bdfad873d763157a2d03d2c935521176
Jason Hall <jhall@ambientdevices.com>
2012-06-07 15:52:50 -0400

    for jill

--- tornado_test.py
+++ tornado_test.py
@@ -65,7 +65,8 @@ class PostHandler(BaseHandler):
                                <input type="text" name="message">
                                <input type="submit" value="Submit">
                            </form>
-                           Hi JILL thanks for entering %s \n LOVE you!
+                           Hi JILL thanks for entering %s
+                           LOVE you!
                         </body>
                     </html>""" % self.get_argument("message"))
                    #"You wrote " + self.get_argument("message"))
